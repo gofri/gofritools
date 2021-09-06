0e4d745334469066be4a7ebccac1998f808fa8db
Gal Ofri <galmenashofri@gmail.com>
2021-09-06 17:45:32 +0300

    beautiful filter

--- L2/lower/virt_filter.py
+++ L2/lower/virt_filter.py
@@ -5,13 +5,49 @@ from common.res import SearchRes
 from common import ui_tools
 from enum import Enum, auto
 
+class BasicFilter(object):
+    def __init__(self, pattern, wildness, case_sensitive, whole_word, invert):
+        self.pattern = pattern
+        self.wildness = wildness
+        self.case_sensitive = case_sensitive
+        self.whole_word = whole_word
+        self.invert = invert
+        self.compiled = utils.compile_re(pattern, wildness=wildness, case_sensitive=case_sensitive, whole_word=whole_word)
+
+    def match(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def post_did_match(self, *args, **kwargs):
+        pass
+
+class PathFilter(BasicFilter):
+    def match(self, data):
+        # TODO fix for proper handling of paths
+        return bool(self.compiled.search(data)) != self.invert
+
+class TextFilter(BasicFilter):
+    def match(self, data):
+        return bool(self.compiled.search(data)) != self.invert
+
+    def post_did_match(self, index, data, virt_filter):
+        pattern_colored = ui_tools.colored(self.pattern, key='text')
+        virt_filter.text_colored[index] = self.compiled.sub(pattern_colored, virt_filter.text_colored[index])
+
+###
+
 class Filteree(Enum):
     TEXT = auto()
     PATH = auto()
-
+    
     def get_relevant_filteree(self, text, paths):
         options = {self.TEXT:text, self.PATH:paths}
         return options[self]
+    
+    def get_filter(self, *args, **kwargs):
+        options = {self.TEXT:TextFilter, self.PATH:PathFilter}
+        return options[self](*args, **kwargs)
+    
+###
 
 class VirtualFilter(object):
     def __init__(self, prev_output, filteree: Filteree):
@@ -20,21 +56,18 @@ class VirtualFilter(object):
         self.text_colored = list(self.prev_output.texts_colored) # clone for local modification 
         self.paths = self.prev_output.paths
         self.lines = self.prev_output.lines
-        self.filtereeType = filteree
-        self.filteree = filteree.get_relevant_filteree(text=self.text, paths=self.paths)
+        self.filteree = filteree
 
     def filter(self, pattern, wildness, case_sensitive, whole_word, invert, **ignorable):
         res = SearchRes()
-
-        for i, t in enumerate(self.filteree):
+        filteree = self.filteree.get_relevant_filteree(text=self.text, paths=self.paths)
+        for i, t in enumerate(filteree):
             any_pattern = False
             for p in pattern:
-                compiled = utils.compile_re(p, wildness=wildness, case_sensitive=case_sensitive, whole_word=whole_word)
-                if bool(compiled.search(t)) != invert:
+                _filter = self.filteree.get_filter(p, wildness, case_sensitive, whole_word, invert)
+                if _filter.match(t):
                     any_pattern = True
-                    if self.filtereeType == Filteree.TEXT:
-                        pattern_colored = ui_tools.colored(p, key='text')
-                        self.text_colored[i] = compiled.sub(pattern_colored, self.text_colored[i])
+                    _filter.post_did_match(i, t, self)
             if any_pattern:
                 res.add_record(path=self.paths[i], line=self.lines[i], text=self.text[i], text_colored=self.text_colored[i])
 
